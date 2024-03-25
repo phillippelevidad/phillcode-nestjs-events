f797029a044575e3a42af7239ac640dce853430a
Phillippe Santana <phillippelevidad@gmail.com>
2024-03-25 14:37:47 -0300

    First commit

---
+++ src/employees/employees.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { EmployeesController } from './employees.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Employee } from './entities/employee.entity';
+import { ContactInfo } from './entities/contact-info.entity';
+import { CqrsModule } from '@nestjs/cqrs';
+import { QueryHandlers } from './queries';
+import { CommandHandlers } from './commands';
+import { EventHandlers } from './events';
+import { CommonModule } from 'src/common/common.module';
+
+@Module({
+  imports: [
+    CqrsModule,
+    TypeOrmModule.forFeature([Employee, ContactInfo]),
+    CommonModule,
+  ],
+  controllers: [EmployeesController],
+  providers: [...QueryHandlers, ...CommandHandlers, ...EventHandlers],
+})
+export class EmployeesModule {}
